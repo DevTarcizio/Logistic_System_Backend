339cd7d04e867b31ca3feff4a13205f446ba5237
DevTarcizio <devpedrotarcizio@gmail.com>
2026-06-05 23:56:23 -0300

    Conexão com o banco implementada

--- src/logistic_system_api/app.py
+++ src/logistic_system_api/app.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from src.logistic_system_api.core.database import get_session
+from fastapi import Depends, FastAPI
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
+DBsession = Annotated[AsyncSession, Depends(get_session)]
 
 app = FastAPI()
 
+
 @app.get('/')
 def read_root():
-    return {"msg": "Hello World!"}
+    return {'msg': 'Hello World!'}
+
+
+@app.get('/db')
+async def db_test(session: DBsession):
+    result = await session.execute(text('SELECT 1'))
+
+    return {'status': 'ok', 'Result': result.scalar()}
---
+++ src/logistic_system_api/core/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from src.logistic_system_api.core.settings import Settings
+
+settings = Settings()
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    pool_size=5,
+    max_overflow=0,
+    pool_pre_ping=True
+)
+
+async def get_session():
+    async with AsyncSession(engine, expire_on_commit=False) as session:
+        yield session
+
+async def close_engine():
+    await engine.dispose()
