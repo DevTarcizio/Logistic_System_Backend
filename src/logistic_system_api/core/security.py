229c77f15bdc561e6efb1bbc8a2560007d2387ed
DevTarcizio <devpedrotarcizio@gmail.com>
2026-06-08 23:18:25 -0300

    Security with JWT add

--- src/logistic_system_api/core/security.py
+++ src/logistic_system_api/core/security.py
@@ -1,12 +1,20 @@
 from datetime import datetime, timedelta, timezone
+from http import HTTPStatus
 
-from jwt import encode
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt import decode, encode
 from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.logistic_system_api.core.database import get_session
+from src.logistic_system_api.core.models import User
 from src.logistic_system_api.core.settings import Settings
 
 settings = Settings()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/users/login')
 
 pwd_context = PasswordHash.recommended()
 
@@ -24,6 +32,39 @@ def create_access_token(subject: str):
         minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
 
-    payload = {'sub': subject, 'exp': expires}
+    payload = {'sub': str(subject), 'exp': expires}
 
     return encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+
+def decode_access_token(token: str):
+    try:
+        payload = decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM],
+        )
+
+        return payload
+
+    except Exception as e:
+        print(f'Erro: {type(e).__name__}')
+        print(f'Mensagem: {e}')
+        raise
+
+
+async def get_current_user(
+    session: AsyncSession = Depends(get_session), token: str = Depends(oauth2_scheme)
+):
+    payload = decode_access_token(token)
+    user_id = payload.get('sub')
+
+    if not user_id:
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail='Invalid token')
+
+    user = await session.scalar(select(User).where(User.id == int(user_id)))
+
+    if not user:
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail='User not found')
+
+    return user
--- src/logistic_system_api/routers/users.py
+++ src/logistic_system_api/routers/users.py
@@ -2,19 +2,20 @@ from http import HTTPStatus
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.logistic_system_api.core.database import get_session
 from src.logistic_system_api.core.models import User
 from src.logistic_system_api.core.schemas import (
-    LoginRequest,
     Token,
     UserCreate,
     UserPublic,
 )
 from src.logistic_system_api.core.security import (
     create_access_token,
+    get_current_user,
     get_password_hash,
     verify_password,
 )
@@ -22,6 +23,7 @@ from src.logistic_system_api.core.security import (
 router = APIRouter(prefix='/users', tags=['users'])
 
 DBsession = Annotated[AsyncSession, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 @router.post('/register', response_model=UserPublic, status_code=HTTPStatus.CREATED)
@@ -54,9 +56,11 @@ async def register_user(user: UserCreate, session: DBsession):
 
 
 @router.post('/login', response_model=Token, status_code=HTTPStatus.OK)
-async def login_user(credentials: LoginRequest, session: DBsession):
+async def login_user(
+    session: DBsession, form_data: OAuth2PasswordRequestForm = Depends()
+):
     db_user = await session.scalar(
-        select(User).where(User.username == credentials.username)
+        select(User).where(User.username == form_data.username)
     )
 
     if not db_user:
@@ -64,11 +68,16 @@ async def login_user(credentials: LoginRequest, session: DBsession):
             status_code=HTTPStatus.UNAUTHORIZED, detail='Invalid username or password'
         )
 
-    if not verify_password(credentials.password, db_user.password):
+    if not verify_password(form_data.password, db_user.password):
         raise HTTPException(
             status_code=HTTPStatus.UNAUTHORIZED, detail='Invalid username or password'
         )
 
-    access_token = create_access_token(subject=db_user.username)
+    access_token = create_access_token(subject=str(db_user.id))
 
     return Token(access_token=access_token, token_type='bearer')
+
+
+@router.get('/me', status_code=HTTPStatus.OK)
+async def read_current_user(current_user: CurrentUser):
+    return current_user
